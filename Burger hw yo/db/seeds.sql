aea5c7a2cb1f287b3905bdfbb984f6f3ea3926fe
kidpk <joshajoyce@gmail.com>
2020-04-06 19:24:06 -0400

    Started burger hw, used the cats activity as a base. Not on heroku yet, never give up

---
+++ Burger hw yo/db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Cheese Burger');
+INSERT INTO burgers (name) VALUES ('Bacon Burger');
+INSERT INTO burgers (name, devoured) VALUES ('Texas Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('BLT Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Turkey Burger', true);
+INSERT INTO burgers (name) VALUES ('BBQ Burger');
